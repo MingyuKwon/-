6f0afe0aefeae35b0b22679e7025da5e9845446b
Kwon Mingyu <kmgmg230601@gmail.com>
2023-11-05 19:12:50 +0900

    전체 설정화면 제작 완료. 이제 초기화 작업을 해주자

--- Custom/Script/UI/OptionPanel.cs
+++ Custom/Script/UI/OptionPanel.cs
@@ -16,9 +16,41 @@ public class OptionPanel : MonoBehaviour
         int value = dropdown.value;
         ResolutionManager.SetResolution(value);
     }
+    public void LanguageChange(Dropdown dropdown)
+    {
+        int value = dropdown.value;
+        
+    }
 
     public void BrightnessChange(float value)
     {
         BrightnessManager.instance.setBrightNess(value);
+    }
+
+    public void BackGroundSoundChange(float value)
+    {
+        GameAudioManager.currentBackGroundVolume = value;
+    }
+
+    public void SFXSoundChange(float value)
+    {
+        GameAudioManager.currentSFXVolume = value;
+    }
+
+    public void UISoundChange(float value)
+    {
+        GameAudioManager.currentUIVolume = value;
+    }
+
+    public void Mute()
+    {
+        if(GameAudioManager.totalVolme > 0)
+        {
+            GameAudioManager.totalVolme = 0;
+        }else
+        {
+            GameAudioManager.totalVolme = 1;
+        }
+        
     }
 }
